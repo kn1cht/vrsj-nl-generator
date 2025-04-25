a48162b9fa00fb2d6e6a4cd22a892a96cc138e4d
kn1cht <16277200+kn1cht@users.noreply.github.com>
2025-04-26 01:04:20 +0900

    disallow half-width space before paragraph

--- src/components/TextRules.tsx
+++ src/components/TextRules.tsx
@@ -28,7 +28,7 @@ const TEXT_RULES: TextRule[] = [
     id: 'paragraph-indent',
     name: '段落頭字下げ',
     description: '句点を含む段落の先頭に全角スペースを挿入します',
-    pattern: /(^|\n)([^\u3000\s])(.*?[。．.！!？?]+.*?)(?=\n|$)/g,
+    pattern: /(^|\n)[ \t]*([^\u3000\s])(.*?[。．.！!？?]+.*?)(?=\n|$)/g,
     replace: (_match: string, p1: string, p2: string, p3: string) => {
       return `${p1}\u3000${p2}${p3}`;
     }
